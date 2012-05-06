c350e41b1e91af67e9a9309cd20fb814f54750bc
alvarodr <alvarodr@hotmail.com>
2012-05-06 16:21:41 +0200

    Actualización Test

--- src/test/java/com/phdhub/oauth/test/TestAuthorization.java
+++ src/test/java/com/phdhub/oauth/test/TestAuthorization.java
@@ -1,9 +1,14 @@
 package com.phdhub.oauth.test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import oauth.signpost.exception.OAuthCommunicationException;
 import oauth.signpost.exception.OAuthExpectationFailedException;
@@ -15,14 +20,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.google.gson.Gson;
+import com.phdhub.mendeley.bom.Group;
+import com.phdhub.mendeley.bom.People;
+import com.phdhub.mendeley.bom.PublicGroupsQuery;
+import com.phdhub.mendeley.bom.Users;
 import com.phdhub.oauth.service.Authorization;
+import com.phdhub.oauth.service.MendeleyService;
+import com.phdhub.ouauth.service.impl.MendeleyServiceImpl;
 
 public class TestAuthorization {
 
 	private Authorization auth;
 	
 	@Before
-	public void prepare() throws IOException{
+	public void prepare() throws IOException, OAuthMessageSignerException, OAuthNotAuthorizedException, OAuthExpectationFailedException, FailingHttpStatusCodeException, OAuthCommunicationException{
 		auth = new Authorization();
 	}
 	
@@ -33,5 +45,63 @@ public class TestAuthorization {
 		HttpURLConnection urlConnect = auth.getTokenOauth(url);
 		urlConnect.connect();
 		Assert.assertNotNull(urlConnect.getInputStream());
+	}
+	
+	@Test
+	public void publicMethodMendeley() throws IOException, FailingHttpStatusCodeException, OAuthMessageSignerException, OAuthExpectationFailedException, OAuthCommunicationException{
+		MendeleyService mendeleyService = new MendeleyServiceImpl();
+		String groups = mendeleyService.getResponseMendeley("http://api.mendeley.com/oapi/documents/groups/");
+		Assert.assertNotNull(groups);
+		
+		
+		Gson gson = new Gson();
+		PublicGroupsQuery publicGroups = gson.fromJson(groups, PublicGroupsQuery.class);
+		Assert.assertNotNull(publicGroups);
+		
+		ArrayList<Group> gr = publicGroups.getGroups();
+		for (Iterator<Group> iterator = gr.iterator(); iterator.hasNext();) {
+			Group group = (Group) iterator.next();
+			String members = mendeleyService.getResponseMendeley("http://api.mendeley.com/oapi/documents/groups/" + group.getId() + "/people");
+			People people = gson.fromJson(members, People.class);
+			
+			if (people.getAdmins()!=null && people.getAdmins().size()>0){
+				ArrayList<Users> users = people.getAdmins();
+				for (Iterator iterator2 = users.iterator(); iterator2.hasNext();) {
+					Users users2 = (Users) iterator2.next();
+					URL url = new URL("http://api.mendeley.com/oapi/profiles/info/" + users2.getUser_id() + "/");
+					HttpURLConnection urlConnect = auth.getTokenOauth(url);
+					BufferedReader bf = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+					String resp = bf.readLine();
+					System.out.println(resp);
+					urlConnect.disconnect();
+				}
+			}
+			
+			if (people.getMembers()!=null && people.getMembers().size()>0){
+				ArrayList<Users> users = people.getMembers();
+				for (Iterator iterator2 = users.iterator(); iterator2.hasNext();) {
+					Users users2 = (Users) iterator2.next();
+					URL url = new URL("http://api.mendeley.com/oapi/profiles/info/" + users2.getUser_id() + "/");
+					HttpURLConnection urlConnect = auth.getTokenOauth(url);
+					BufferedReader bf = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+					String resp = bf.readLine();
+					System.out.println(resp);
+					urlConnect.disconnect();
+				}
+			}
+			
+			if (people.getFollowers()!=null && people.getFollowers().size()>0){
+				ArrayList<Users> users = people.getFollowers();
+				for (Iterator iterator2 = users.iterator(); iterator2.hasNext();) {
+					Users users2 = (Users) iterator2.next();
+					URL url = new URL("http://api.mendeley.com/oapi/profiles/info/" + users2.getUser_id() + "/");
+					HttpURLConnection urlConnect = auth.getTokenOauth(url);
+					BufferedReader bf = new BufferedReader(new InputStreamReader(urlConnect.getInputStream()));
+					String resp = bf.readLine();
+					System.out.println(resp);
+					urlConnect.disconnect();
+				}
+			}
+		}
 	}
 }
